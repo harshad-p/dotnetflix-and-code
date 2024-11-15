65b86f0a03ff1f15e963bebedcd7681dd6b2477e
harshad-p <harshad.mscs@gmail.com>
2024-11-15 20:56:06 +0530

    Add UnpackNumber project

---
+++ UnpackNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string input; 
+
+Console.WriteLine("Enter an integer.");
+input = Console.ReadLine();
+
+Console.WriteLine($"You entered: " + input);
+
+for(int i = input.Length - 1; i >= 0 ; i--){
+    var num = int.Parse(input[i].ToString());
+    var unpacked = num * Math.Pow(10, i);
+    //Console.Write(unpacked + " ");
+}
+
+for(int i = 0; i < input.Length; i++){
+    var num = int.Parse(input[i].ToString());
+    var unpacked = num * Math.Pow(10, input.Length - i - 1);
+    Console.Write(unpacked + " ");
+}
